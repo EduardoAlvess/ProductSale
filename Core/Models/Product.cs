592b70fafff98c2bf7e2f5f2c544fd309d362c9a
Eduardo Alves <eduardo.silva@umbler.com>
2023-07-11 11:52:21 -0300

    first commit

---
+++ Core/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductSale.Core.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public int AmountInStock { get; set; }
+
+    }
+}
