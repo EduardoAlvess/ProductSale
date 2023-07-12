f5600600ee2cbce2f1fcafb80bcd1cae8df455f7
Eduardo Alves <eduardo.silva@umbler.com>
2023-07-12 12:15:55 -0300

    isDeleted column in products

--- Core/Models/Product.cs
+++ Core/Models/Product.cs
@@ -8,6 +8,7 @@ namespace ProductSale.Core.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public double Value { get; set; }
+        public bool isDeleted { get; set; }
         public int AmountInStock { get; set; }
         public string Description { get; set; }
         public double ProductionCost { get; set; }
---
+++ Migrations/20230712151425_CreateIsDeletedColumnInProductsTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductSale.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIsDeletedColumnInProductsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Products",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Products");
+        }
+    }
+}
