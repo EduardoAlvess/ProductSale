b7f502f3fdf4ff0a810a7f6dbecd94cca4599341
Eduardo Alves <eduardo.silva@umbler.com>
2023-07-12 13:16:49 -0300

    New exception

---
+++ Core/Exceptions/ProductExceptions/ProductNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductSale.Core.Exceptions.ProductExceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public override string StackTrace => string.Empty;
+
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
