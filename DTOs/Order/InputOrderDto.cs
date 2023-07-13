3a2f7a7cc11ea385ada50780a97fa0f613f91cc3
Eduardo Alves <eduardo.silva@umbler.com>
2023-07-12 22:07:38 -0300

    Order DTOs

---
+++ DTOs/Order/InputOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using ProductSale.Core.Enums;
+using ProductSale.DTOs.Product;
+
+namespace ProductSale.DTOs.Order
+{
+    public class InputOrderDto
+    {
+        public Stage Stage { get; set; }
+        public double Amount { get; set; }
+        public double Profit { get; set; }
+        public int CustomerId { get; set; }
+        public ICollection<OutputProductDto> Products { get; set; }
+    }
+}
