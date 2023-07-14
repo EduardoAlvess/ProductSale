2716f770c598b208b120a30c39b184eefe246d36
Eduardo Alves <eduardo.silva@umbler.com>
2023-07-14 18:20:17 -0300

    Order validator

--- App/Validations/InputOrderValidator.cs
+++ App/Validations/InputOrderValidator.cs
@@ -1,17 +1,61 @@
 ﻿using FluentValidation;
+using ProductSale.Core.Models;
 using ProductSale.DTOs.Orders;
+using ProductSale.Infra.DB;
+using System.Linq;
 
 namespace ProductSale.App.Validations
 {
     public class InputOrderValidator : AbstractValidator<InputOrderDto>
     {
-        public InputOrderValidator()
+        private readonly IDbContext _db;
+
+        public InputOrderValidator(IDbContext db)
         {
+            _db = db;
+
             RuleFor(m => m.Stage)
                 .NotNull()
                     .WithMessage("Stage must not be null")
                 .IsInEnum()
                     .WithMessage("Stage must be valid");
+
+            RuleFor(m => m.Amount)
+                .GreaterThanOrEqualTo(0)
+                    .WithMessage("Amount must not be less than 0");
+
+            RuleForEach(m => m.OrderProducts).ChildRules(c =>
+            {
+                c.RuleFor(x => x.Quantity)
+                    .GreaterThanOrEqualTo(1)
+                        .WithMessage("The quantity must be equal or greater than 1")
+                    .LessThanOrEqualTo(x => GetAmountInStock(x.ProductId))
+                        .WithMessage("The quantity is higher than the amount in stock");
+
+                c.RuleFor(x => x.ProductId)
+                    .Must(DoesProductExist)
+                        .WithMessage("The product don't exist");
+            });
+
+            RuleFor(m => m.CustomerId)
+                .Must(DoesUserExist)
+                    .WithMessage("User don't exist");
         }
+
+        private bool DoesProductExist(int productId)
+        {
+            var productsIds = _db.Products.Select(c => c.Id).ToList();
+
+            return productsIds.Contains(productId) ? true : false;
+        }
+
+        private bool DoesUserExist(int customerId)
+        {
+            var customersIds = _db.Customers.Select(c => c.Id).ToList();
+
+            return customersIds.Contains(customerId) ? true : false;
+        }
+
+        private int GetAmountInStock(int productId) => _db.Products.First(p => p.Id == productId).AmountInStock;
     }
 }
