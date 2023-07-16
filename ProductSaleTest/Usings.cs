4266cc6425a228614fe35bfe38b3ef8d82ed8f68
Eduardo Alves <33400425+EduardoAlvess@users.noreply.github.com>
2023-07-16 00:47:56 -0300

    fixing

--- ProductSaleTest/Usings.cs
+++
@@ -1,6 +0,0 @@
-global using Microsoft.AspNetCore.JsonPatch;
-global using Microsoft.EntityFrameworkCore;
-global using ProductSale.Core.Models;
-global using ProductSale.Infra.DB;
-global using Xunit;
-global using Moq;
