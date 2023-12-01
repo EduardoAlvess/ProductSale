377f6dbff6d339314cc778375b261adaff896a25
Eduardo4lvess <EduardoAlves@DESKTOP-2BCTCA1>
2023-12-01 15:10:02 -0300

    fixing pipeline

--- ProductSale/Program.cs
+++ ProductSale/Program.cs
@@ -53,11 +53,14 @@ namespace ProductSale
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
 
             app.UseAuthorization();
 
